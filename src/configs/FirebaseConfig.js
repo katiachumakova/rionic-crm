431ca2626bf2d47739dcccd55aa5e3fe1a3ffda5
katiachumakova <katiachumakova95@gmail.com>
2020-09-10 14:03:10 -0500

    initial commit

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,22 @@
+// export default {
+//   apiKey: 'AIzaSyAOWoRQGoegkmwnCca7B2GG5GUG_ZP1lcM',
+//   authDomain: 'emilus.firebaseapp.com',
+//   databaseURL: 'https://emilus.firebaseio.com',
+//   projectId: 'emilus',
+//   storageBucket: 'emilus.appspot.com',
+//   messagingSenderId: '807555350717',
+//   appId: '1:807555350717:web:145ba7c21af47203a2f7d4',
+//   measurementId: 'G-8KE7HJB252'
+// };
+
+
+export default {
+	  apiKey: "AIzaSyB8UaUIYW9gJALBhnNqUjUqEuqR4fKtyTQ",
+    authDomain: "rionic-82311.firebaseapp.com",
+    databaseURL: "https://rionic-82311.firebaseio.com",
+    projectId: "rionic-82311",
+    storageBucket: "rionic-82311.appspot.com",
+    messagingSenderId: "262397409021",
+    appId: "1:262397409021:web:9025a281a8558439bfb094",
+    measurementId: "G-275GQNR8SZ"
+};
